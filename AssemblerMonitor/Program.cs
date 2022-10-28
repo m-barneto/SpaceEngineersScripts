cc67bf6515a92c97f87e27cb60e24a3be30acbdd
mbarneto <mattdokn@gmail.com>
2022-10-28 09:59:17 -0400

    Changes

--- AssemblerMonitor/Program.cs
+++ AssemblerMonitor/Program.cs
@@ -21,93 +21,161 @@ using VRageMath;
 namespace IngameScript {
     partial class Program : MyGridProgram {
         class QueuedItem {
-            public bool active;
             public MyFixedPoint amount;
+            public DateTime lastWorked;
 
-            public QueuedItem(MyFixedPoint amount) {
+            public QueuedItem(MyFixedPoint amount, DateTime lastWorked) {
                 this.amount = amount;
-                this.active = false;
+                this.lastWorked = lastWorked;
+            }
+
+            public bool IsActive(DateTime currentTick) {
+                return (currentTick - lastWorked).TotalSeconds <= 10.0;
             }
         }
 
         List<IMyAssembler> assemblers;
         List<IMyTextPanel> panels;
-        Dictionary<MyDefinitionId, QueuedItem> prodQueue, dissQueue;
+
+        Dictionary<MyDefinitionId, QueuedItem> currAssemblerQueue, prevAssemblerQueue;
 
         public Program() {
             assemblers = new List<IMyAssembler>();
             GridTerminalSystem.GetBlocksOfType(assemblers);
             if (assemblers.Count == 0) {
-                Echo("No assemblers found!");
-                return;
+                throw new Exception("No assemblers found on grid.");
             }
 
             panels = new List<IMyTextPanel>();
             GridTerminalSystem.GetBlocksOfType(panels, panel => panel.CustomName.Contains("[Assembler]"));
             if (panels.Count == 0) {
-                Echo("No LCD's found!");
-                return;
+                throw new Exception("No LCDs found on grid. Tag an LCD with [Assembler] to use with this script.");
             }
 
-            prodQueue = new Dictionary<MyDefinitionId, QueuedItem>();
-            dissQueue = new Dictionary<MyDefinitionId, QueuedItem>();
+            currAssemblerQueue = new Dictionary<MyDefinitionId, QueuedItem>();
+            prevAssemblerQueue = new Dictionary<MyDefinitionId, QueuedItem>();
 
             Runtime.UpdateFrequency = UpdateFrequency.Update10;
         }
 
         public void Main(string argument, UpdateType updateSource) {
-            prodQueue.Clear();
-            dissQueue.Clear();
+            // Runs every 10 ticks
 
+            // Go through each assembler and add their queues to a master list
+            currAssemblerQueue.Clear();
             List<MyProductionItem> items = new List<MyProductionItem>();
-
             for (int i = 0; i < assemblers.Count; ++i) {
                 items.Clear();
                 assemblers[i].GetQueue(items);
                 if (assemblers[i].Mode == MyAssemblerMode.Assembly) {
                     for (int j = 0; j < items.Count; ++j) {
-                        // Item already in dict, check if decreased count to mark as active or its count has increased
-                        if (prodQueue.ContainsKey(items[j].BlueprintId)) {
-
-                            if (prodQueue[items[j].BlueprintId].amount != items[j].Amount) {
-                                // amount changed, if its less than previous, flag as active
+                        
+                        // Item already in dict, just add to the amount
+                        if (currAssemblerQueue.ContainsKey(items[j].BlueprintId)) {
+                            currAssemblerQueue[items[j].BlueprintId].amount += items[j].Amount;
+                        } else {
+                            // Item not in dict, create new QueuedItem and add it.
+                            // TODO: Add timestamp to newly created QueuedItem, for future pruning
+                            if (prevAssemblerQueue.ContainsKey(items[j].BlueprintId)) {
+                                currAssemblerQueue.Add(items[j].BlueprintId, new QueuedItem(items[j].Amount, prevAssemblerQueue[items[j].BlueprintId].lastWorked));
+                            } else {
+                                currAssemblerQueue.Add(items[j].BlueprintId, new QueuedItem(items[j].Amount, DateTime.MinValue));
                             }
-                            prodQueue[items[j].BlueprintId].amount += items[j].Amount;
-                        } else {
-                            prodQueue.Add(items[j].BlueprintId, new QueuedItem(items[j].Amount));
-                        }
-                    }
-                } else {
-                    for (int j = 0; j < items.Count; ++j) {
-                        if (dissQueue.ContainsKey(items[j].BlueprintId)) {
-                            dissQueue[items[j].BlueprintId].amount += items[j].Amount;
-                        } else {
-                            dissQueue.Add(items[j].BlueprintId, new QueuedItem(items[j].Amount, items[j].ItemId == assemblers[i].NextItemId));
                         }
                     }
                 }
             }
 
+            // Compare master list to previous runs list and check for active items
+            if (prevAssemblerQueue.Count > 0) {
+                foreach (var item in currAssemblerQueue) {
+                    // If item in current list is present in prev list
+                    if (prevAssemblerQueue.ContainsKey(item.Key)) {
+                        // If amount decreased mark as active
+                        if (item.Value.amount < prevAssemblerQueue[item.Key].amount) {
+                            item.Value.lastWorked = DateTime.UtcNow;
+                        }
+                    } else {
+                        // New item queued, doesn't necessarily mean it's active though
+                    }
+                }
+            }
 
-            /*foreach (var panel in panels) {
+            // Replace prev list with the master list
+            prevAssemblerQueue.Clear();
+            foreach (var item in currAssemblerQueue) {
+                prevAssemblerQueue.Add(item.Key, item.Value);
+            }
+
+            // Sort the dict
+            List<KeyValuePair<MyDefinitionId, QueuedItem>> myList = currAssemblerQueue.ToList();
+            myList.Sort(
+                delegate(KeyValuePair<MyDefinitionId, QueuedItem> item1, KeyValuePair<MyDefinitionId, QueuedItem> item2) {
+                    if (item1.Value.IsActive(DateTime.UtcNow)) {
+                        if (item2.Value.IsActive(DateTime.UtcNow)) {
+                            return item1.Value.amount.ToIntSafe().CompareTo(item2.Value.amount.ToIntSafe());
+                        } else return -1;
+                    }
+                    else return item1.Value.amount.ToIntSafe().CompareTo(item2.Value.amount.ToIntSafe());
+                }
+            );
+
+
+            foreach (var panel in panels) {
+                panel.ContentType = ContentType.SCRIPT;
+                panel.Script = "";
+
+                var frame = panel.DrawFrame();
+
+                RectangleF viewport = new RectangleF((panel.TextureSize - panel.SurfaceSize) / 2f, panel.SurfaceSize);
+
+                DrawFrame(ref frame, viewport, myList);
+
+                frame.Dispose();
+
+
                 continue;
-                foreach (var item in prodQueue) {
-                    panel.WriteText(string.Format("{0,-15} | {1} {2}\n", GetSanitizedName(item.Key.SubtypeName), item.Value.amount.ToIntSafe(), item.Key.), true);
-                }
                 panel.WriteText("Items in Queue\n");
-                if (prodQueue.Count > 0) {
+                if (currAssemblerQueue.Count > 0) {
                     panel.WriteText("Assembling:\n", true);
-                    foreach (var item in prodQueue) {
-                        panel.WriteText(string.Format("{0,-15} | {1} {2}\n", GetSanitizedName(item.Key.SubtypeName), item.Value.amount.ToIntSafe(), item.Value.active ? "T" : "F"), true);
+                    foreach (var item in currAssemblerQueue) {
+                        panel.WriteText(string.Format("{0,-15} | {1} {2} {3}\n", GetSanitizedName(item.Key.SubtypeName), item.Value.amount.ToIntSafe(), item.Value.IsActive(DateTime.UtcNow) ? "T" : "F", item.Value.lastWorked), true);
                     }
                 }
-                if (dissQueue.Count > 0) {
-                    panel.WriteText("Disassembling:\n", true);
-                    foreach (var item in dissQueue) {
-                        panel.WriteText(string.Format("{0,-15} | {1} {2}\n", GetSanitizedName(item.Key.SubtypeName), item.Value.amount.ToIntSafe(), item.Value.active ? "T" : "F"), true);
-                    }
-                }
-            }*/
+            }
+        }
+
+        void DrawFrame(ref MySpriteDrawFrame frame, RectangleF viewport, List<KeyValuePair<MyDefinitionId, QueuedItem>> items) {
+            var pos = new Vector2(5, 0) + viewport.Position;
+
+            var sprite = new MySprite() {
+                Type = SpriteType.TEXT,
+                Data = "Assembling:",
+                Position = pos,
+                RotationOrScale = 1.5f,
+                Color = Color.White,
+                Alignment = TextAlignment.LEFT,
+                FontId = "White"
+            };
+            frame.Add(sprite);
+            pos += new Vector2(0, 40);
+
+
+            foreach (var item in items) {
+                //panel.WriteText(string.Format("{0,-15} | {1} {2} {3}\n", GetSanitizedName(item.Key.SubtypeName), item.Value.amount.ToIntSafe(), item.Value.IsActive(DateTime.UtcNow) ? "T" : "F", item.Value.lastWorked), true);
+                sprite = new MySprite() {
+                    Type = SpriteType.TEXT,
+                    Data = string.Format("{0,-15} | {1}\n", GetSanitizedName(item.Key.SubtypeName), item.Value.amount.ToIntSafe(), item.Value.lastWorked),
+                    Position = pos,
+                    RotationOrScale = 1.0f,
+                    Color = item.Value.IsActive(DateTime.UtcNow) ? Color.Green : Color.Red,
+                    Alignment = TextAlignment.LEFT,
+                    FontId = "White"
+                };
+                frame.Add(sprite);
+
+                pos += new Vector2(0, 22);
+            }
         }
 
         string GetSanitizedName(string name) {
